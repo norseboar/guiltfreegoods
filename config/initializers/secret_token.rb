3170a0f93a4d6cb034d06172a190fde6a1a979ef
reed <reed@Kane.(none)>
2012-09-09 14:10:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guiltfreegoods::Application.config.secret_token = 'cb75d56fbbc4766e25d5124d4400d2f8f921ff63630dee3bdd0295d18c0e8fb5daa6df9393ca9c630978b47753516516329e9986c84b5efc612d9391ac562757'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Guiltfreegoods::Application.config.session_store :cookie_store, key: '_guiltfreegoods_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Guiltfreegoods::Application.config.session_store :active_record_store
