5db070576f9c83d3a1c983f4a931d4913e8c5070
NorseBoar <reed@outlook.com>
2012-09-14 03:46:15 -0500

    fix scss error, improve routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,33 @@ describe "Static Pages" do
   
   describe "Home page" do
     it "should have the correct title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Guilt-Free Goods")
     end
     it "should have the content 'Guilt-Free Goods'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Guilt-Free Goods')
     end
   end
 
   describe "Help page" do
     it "should have the correct title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "Guilt-Free Goods | Help")
     end
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
 
   describe "About page" do
     it "should have the correct title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => "Guilt-Free Goods | About")
     end
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
   end
